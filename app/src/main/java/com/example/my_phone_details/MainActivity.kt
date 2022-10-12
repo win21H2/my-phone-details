8396434843d8929e1d05f99f8b64896db0fc2095
324Hz <1024Bitcoins@gmail.com>
2022-10-11 22:57:40 -0500

    commit

--- app/src/main/java/com/example/my_phone_details/MainActivity.kt
+++ app/src/main/java/com/example/my_phone_details/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.my_phone_details
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.WindowCompat
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = this.resources.getColor(R.color.main_background)
+        window.navigationBarColor = this.resources.getColor(R.color.main_background)
+
+        val hardwarePage: Button = findViewById(R.id.hardware)
+        hardwarePage.setOnClickListener {
+            hardwarePage()
+        }
+
+        val softwarePage: Button = findViewById(R.id.software)
+        softwarePage.setOnClickListener {
+            softwarePage()
+        }
+    }
+
+    private fun hardwarePage() {
+        Toast.makeText(this, "hardwarePage", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, hardwarePage::class.java); startActivity(intent)
+    }
+    private fun softwarePage() {
+        Toast.makeText(this, "softwarePage", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, softwarePage::class.java); startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/my_phone_details/softwarePage.kt
@@ -0,0 +1,11 @@
+package com.example.my_phone_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class softwarePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_software_page)
+    }
+}
