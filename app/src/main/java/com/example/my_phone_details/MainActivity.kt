dafd325529f49bc485d8175fde4a98c5a8c17d8a
324Hz <1024Bitcoins@gmail.com>
2022-10-11 22:08:38 -0500

    commit

--- app/src/main/java/com/example/my_phone_details/MainActivity.kt
+++ app/src/main/java/com/example/my_phone_details/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.my_phone_details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
